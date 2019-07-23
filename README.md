c05d24f4da83c77df51c7bc120c6fd062504600b
surusathish <surusathish@gmail.com>
2019-07-23 12:49:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nyfefo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nyfefo)
---
+++ src/app/heading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'HC',
+  templateUrl: '<h1>{{hContent}}</h1>'
+})
+
+export class HeadingComponent {
+  hContent: 'Another Component'
+}
